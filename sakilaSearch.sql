4761391d9ee503082dd68ed052db039e2f933dfb
Joshua Lopez <jrenelopez92@gmail.com>
2021-05-13 09:30:20 -0500

    first commit

---
+++ sakilaSearch.sql
@@ -0,0 +1,14 @@
+--1Find the surname of the actor with the forename 'John'.
+select last_name from actor where first_name="John";
+
+-- 2 Find all actors with surname 'Neeson'.
+select last_name, first_name from actor where last_name="Neeson";
+
+-- 3 Find all actors with ID numbers divisible by 10.
+
+
+
+
+
+
+
