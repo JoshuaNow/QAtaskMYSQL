28f92d16bd75b00e1773b180646e85cfcc80d2df
Joshua Lopez <jrenelopez92@gmail.com>
2021-05-13 10:57:02 -0500

    moving on to the next task

--- sakilaSearch.sql
+++ sakilaSearch.sql
@@ -1,12 +1,25 @@
---1Find the surname of the actor with the forename 'John'.
+-- 1) Find the surname of the actor with the forename 'John'.
 select last_name from actor where first_name="John";
 
--- 2 Find all actors with surname 'Neeson'.
+-- 2) Find all actors with surname 'Neeson'.
 select last_name, first_name from actor where last_name="Neeson";
 
--- 3 Find all actors with ID numbers divisible by 10.
+-- 3) Find all actors with ID numbers divisible by 10.
+select actor_id , first_name, last_name from actor where (actor_id % 10) = 0;
 
+-- 4) What is the description of the movie with an ID of 100?
+select description from film_list where FID =100; 
 
+-- 5) Find every R-rated movie.
+select title, rating from film_list where rating="R";
+
+-- 6) Find every non-R-rated movie.
+select title, rating from film_list where rating != "R";
+-- 7) Find the ten shortest movies.
+select title, length from film_list order by length asc limit 10;
+-- 8) Find all movies that have deleted scenes.
+select special_features , title from film where special_features="Deleted Scenes";
+-- 9) Find the titles of all the horror movies.
 
 
 
